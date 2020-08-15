a95805c004f09710e41e62c4f42507806907c4de
Metzo00 <44703498+Metzo00@users.noreply.github.com>
2020-08-15 23:53:04 +0100

    a few changes

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>My Free Movie App</h1>
+        <h1>Free Movie Search App </h1>
       </header>
       <main>
         <Search handleInput={handleInput} search={search} />
